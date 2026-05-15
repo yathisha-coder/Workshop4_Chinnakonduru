b89ed35e6fc5abcfd0613af16e4a0388a0286105
yathisha-coder <yathisai85@gmail.com>
2026-05-15 13:10:13 -0400

    Implement the LeaseContract class and implement the code and logic

--- src/main/java/org/example/LeaseContract.java
+++ src/main/java/org/example/LeaseContract.java
@@ -1,4 +1,34 @@
 package org.example;
 
-public class LeaseContract {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class LeaseContract extends Contract{
+    public LeaseContract(String date,String customerName, String customerEmail,Vehicle vehicleSold){
+        super(date,customerName,customerEmail,vehicleSold);
+    }
+
+    @Override
+    public BigDecimal getTotalPrice() {
+        return getExpectedEndingValue().add(getLeaseFee()).setScale(2,RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public BigDecimal getMonthlyPayment() {
+        BigDecimal total = getTotalPrice();
+        double monthlyRate = 0.04/12;
+        int months = 36;
+        double payment = (total.doubleValue()*monthlyRate)/(1-Math.pow(1+monthlyRate, -months));
+        return BigDecimal.valueOf(payment).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal getExpectedEndingValue(){
+        return BigDecimal.valueOf(getVehicleSold().getPrice()).multiply(BigDecimal.valueOf(0.5))
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+    public BigDecimal getLeaseFee(){
+        return BigDecimal.valueOf(getVehicleSold().getPrice())
+                .multiply(BigDecimal.valueOf(0.07)).setScale(2,RoundingMode.HALF_UP);
+    }
+
 }
