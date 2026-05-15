ff7bb6c8dba4faf6d9f6ed972efef449eb0ccf19
yathisha-coder <yathisai85@gmail.com>
2026-05-15 10:42:58 -0400

    Implement the Contract, Lease and Sales classes. In the contract class implement the abstract class and methods.

---
+++ src/main/java/org/example/Contract.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold){
+        this.date = date;
+        this. customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+    public abstract BigDecimal getTotalPrice();
+
+    public abstract BigDecimal getMonthlyPayment();
+
+    public String getDate(){
+        return date;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public String getCustomerName(){
+        return customerName;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+    public String getCustomerEmail(){
+        return  customerEmail;
+    }
+    public void setCustomerEmail(String customerEmail){
+        this.customerEmail = customerEmail;
+    }
+    public Vehicle getVehicleSold(){
+        return vehicleSold;
+    }
+    public void setVehicleSold(Vehicle vehicleSold){
+        this.vehicleSold = vehicleSold;
+    }
+}
